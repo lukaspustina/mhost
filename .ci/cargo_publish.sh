030fde9f11c695fac28d2981be2c33afd3977873
Lukas Pustina <lukas@pustina.net>
2017-09-28 16:01:14 +0200

    Fix 4 for new cargo publish deployment

--- .ci/cargo_publish.sh
+++ .ci/cargo_publish.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+echo "Upgrading OpenSSL because 'cargo publish' runs outside of cross containers and trust dns needs at least openssl 1.0.2"
+wget http://security.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.0.0_1.0.2g-1ubuntu4.6_amd64.deb
+wget http://security.ubuntu.com/ubuntu/pool/main/o/openssl/libssl-dev_1.0.2g-1ubuntu4.6_amd64.deb
+
+sudo dpkg -i libssl1.0.0_1.0.2g-1ubuntu4.6_amd64.deb
+sudo dpkg -i libssl-dev_1.0.2g-1ubuntu4.6_amd64.deb
+
 echo "Cleaning working directory"
 git clean -f -x
 echo "Publishing to crates.io"
