58e7e328d6e440721598b18096ccabcb7b484cdc
Lukas Pustina <lukas@pustina.net>
2017-10-22 14:03:40 +0200

    Add first integration tests

---
+++ tests/mhost.rs
@@ -0,0 +1,47 @@
+extern crate assert_cli;
+
+#[cfg(test)]
+mod integration {
+    use assert_cli;
+
+    #[test]
+    fn mhost_wo_args() {
+        assert_cli::Assert::command(&["cargo", "run", "--features=bin"])
+            .fails()
+            .and()
+            .stderr().contains("error: The following required arguments were not provided:")
+            .unwrap()
+    }
+
+    #[test]
+    fn mhost_simple_lookup() {
+        assert_cli::Assert::command(&["cargo", "run", "--features=bin"])
+            .with_args(&["www.example.com"])
+            .succeeds()
+            .and()
+            .stdout().contains("* IPv4: 93.184.216.34")
+            .and()
+            .stdout().contains("* IPv6: 2606:2800:220:1:248:1893:25c8:1946")
+            .unwrap()
+    }
+
+    #[test]
+    fn mhost_simple_ipv4_reverse_lookup() {
+        assert_cli::Assert::command(&["cargo", "run", "--features=bin"])
+            .with_args(&["8.8.8.8"])
+            .succeeds()
+            .and()
+            .stdout().contains("* PTR: google-public-dns-a.google.com.")
+            .unwrap()
+    }
+
+    #[test]
+    fn mhost_simple_ipv6_reverse_lookup() {
+        assert_cli::Assert::command(&["cargo", "run", "--features=bin"])
+            .with_args(&["2001:4860:4860::8888"])
+            .succeeds()
+            .and()
+            .stdout().contains("* PTR: google-public-dns-a.google.com.")
+            .unwrap()
+    }
+}
