f3515b145b4949c1b5ed8e5f277e987202a4a394
Lukas Pustina <lukas@pustina.de>
2020-07-04 23:12:08 +0200

    resolver struct with Clone

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use futures::stream::{self, StreamExt};
-use futures::Future;
 use log::{self, *};
 use std::io::Write;
 use std::sync::Arc;
@@ -12,65 +11,58 @@ use trust_dns_resolver::TokioAsyncResolver;
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
-pub struct Resolver {
-    inner: TokioAsyncResolver,
+#[derive(Clone)]
+pub struct MyResolver {
+    inner: Arc<TokioAsyncResolver>,
     name: String,
 }
 
-async fn create_resolver(i: usize) -> Result<TokioAsyncResolver> {
-    let config = match i {
-        1 => ResolverConfig::google(),
-        2 => ResolverConfig::cloudflare(),
-        _ => ResolverConfig::quad9(),
-    };
-    let resolver = TokioAsyncResolver::tokio(config, ResolverOpts::default()).await?;
-    debug!("Created {} Resolver.", i);
+impl MyResolver {
+    pub async fn new(i: usize) -> Result<Self> {
+        let config = match i {
+            1 => ResolverConfig::google(),
+            2 => ResolverConfig::cloudflare(),
+            _ => ResolverConfig::quad9(),
+        };
+        let resolver = TokioAsyncResolver::tokio(config, ResolverOpts::default()).await?;
+        debug!("Created {} Resolver.", i);
 
-    Ok(resolver)
-}
-
-/// Async lookup algorithm -- no work is done
-async fn lookup(i: usize, query: Arc<Query>) -> Result<Vec<Lookup>> {
-    let resolver = create_resolver(i).await?;
-
-    let futures: Vec<_> = query.record_types
-    .iter()
-    .map(|x| make_lookup(i, &resolver, x.clone()))
-    .collect();
-
-    let mut res = Vec::new();
-    let stream = stream::iter(futures);
-    let mut buffered_stream = stream.buffer_unordered(2);
-    while let Some(f) = buffered_stream.next().await {
-        trace!("Received lookup {:?}", f);
-        let f = f?;
-        res.push(f)
+        Ok(MyResolver {
+            inner: Arc::new(resolver),
+            name: i.to_string(),
+        })
     }
 
-    Ok(res)
+    /// Async lookup algorithm -- no work is done
+    pub async fn lookup(self, query: Arc<Query>) -> Result<Vec<Lookup>> {
+        let resolver = Arc::clone(&self.inner);
+
+        let futures: Vec<_> = query.record_types
+            .iter()
+            .map(|x| make_lookup(&self.name, &resolver, x.clone()))
+            .collect();
+
+        let mut res = Vec::new();
+        let stream = stream::iter(futures);
+        let mut buffered_stream = stream.buffer_unordered(2);
+        // TODO: https://rust-lang.github.io/async-book/05_streams/02_iteration_and_concurrency.html
+        while let Some(f) = buffered_stream.next().await {
+            trace!("Received lookup {:?}", f);
+            let f = f?;
+            res.push(f)
+        }
+
+        Ok(res)
+    }
 }
 
-async fn make_lookup(i: usize, resolver: &TokioAsyncResolver, record_type: RecordType) -> Result<Lookup> {
+async fn make_lookup(i: &str, resolver: &TokioAsyncResolver, record_type: RecordType) -> Result<Lookup> {
     let lookup = resolver
         .lookup("www.example.com.", record_type, DnsRequestOptions::default())
         .await?;
     debug!("Received {} Lookup for record type {}.", i, record_type);
 
     Ok(lookup)
-}
-
-/// Create the futures -- no work is done
-fn lookup_futures(query: Arc<Query>) -> Vec<impl Future<Output = Result<Vec<Lookup>>>> {
-    let google = lookup(1, Arc::clone(&query));
-    let cloudflare = lookup(2, Arc::clone(&query));
-    let quad6 = lookup(3, Arc::clone(&query));
-
-    vec![google, cloudflare, quad6]
-}
-
-/// Spawn the futures -- work is started
-fn lookups_spawns(lookup_futures: Vec<impl Future<Output = Result<Vec<Lookup>>> + Send + 'static>) -> Vec<task::JoinHandle<Result<Vec<Lookup>>>> {
-    lookup_futures.into_iter().map(task::spawn).collect()
 }
 
 /// Wait asyncly for the work to finish
@@ -87,10 +79,20 @@ async fn lookups(tasks: Vec<task::JoinHandle<Result<Vec<Lookup>>>>) -> Result<Ve
 async fn do_main(query: Query) -> Result<Vec<Lookup>> {
     let query = Arc::new(query);
 
-    let futures = lookup_futures(query);
+    let google = MyResolver::new(1).await?;
+    let cloudflare = MyResolver::new(2).await?;
+    let quad6 = MyResolver::new(3).await?;
+    debug!("Created all the resolvers");
+
+    let google_l = google.clone().lookup(Arc::clone(&query));
+    let cloudflare_l = cloudflare.clone().lookup(Arc::clone(&query));
+    let quad6_l = quad6.clone().lookup(Arc::clone(&query));
     debug!("Created all the futures");
-    let tasks = lookups_spawns(futures);
+
+    let tasks: Vec<task::JoinHandle<Result<Vec<Lookup>>>> = vec![google_l, cloudflare_l, quad6_l]
+        .into_iter().map(task::spawn).collect();
     debug!("Spawned all the futures");
+
     let lookups = lookups(tasks).await;
     debug!("Awaited all the futures");
 
