40b0a83e2820a0b9c25c4f200dec35b9bea5061e
Lukas Pustina <lukas@pustina.de>
2020-07-15 11:08:39 +0200

    Fix examples

--- examples/system_resolvers.rs
+++ examples/system_resolvers.rs
@@ -15,13 +15,13 @@ async fn main() {
 
     let mq = MultiQuery::new(name, [RecordType::A, RecordType::AAAA, RecordType::TXT])
         .expect("failed to create multi-query");
-    let multi_lookup = resolvers.multi_lookup(mq).await;
+    let lookups = resolvers.multi_lookup(mq).await;
 
-    //println!("Multi-Lookup results: {:#?}", multi_lookup);
+    //println!("Multi-Lookup results: {:#?}", lookups);
 
-    let successes = multi_lookup.iter().filter(|x| x.result().is_ok()).count();
+    let successes = lookups.iter().filter(|x| x.result().is_ok()).count();
     println!("Multi-Lookup successful results: {}/{}", successes, lookups.len());
 
-    let failures: Vec<_> = multi_lookup.iter().filter(|x| !x.result().is_ok()).collect();
+    let failures: Vec<_> = lookups.iter().filter(|x| !x.result().is_ok()).collect();
     println!("Multi-Lookup failed results: {:#?}", failures);
 }
