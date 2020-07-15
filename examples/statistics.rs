b2ac5dc81da1a9efd68f1fa59ccc54bfddd7897b
Lukas Pustina <lukas@pustina.de>
2020-07-15 22:47:47 +0200

    Add basic result statistics

---
+++ examples/statistics.rs
@@ -0,0 +1,27 @@
+use mhost::resolver::ResolverGroup;
+use mhost::{MultiQuery, RecordType};
+use std::env;
+
+#[tokio::main]
+async fn main() {
+    let name = env::args()
+        .skip(1)
+        .next()
+        .unwrap_or_else(|| "www.example.com".to_string());
+
+    let resolvers = ResolverGroup::from_system_config(Default::default())
+        .await
+        .expect("failed to create system resolvers");
+
+    let mq = MultiQuery::new(name, [RecordType::A, RecordType::AAAA, RecordType::TXT])
+        .expect("failed to create multi-query");
+    let lookups = resolvers.multi_lookup(mq).await;
+
+    //println!("Multi-Lookup results: {:#?}", lookups);
+
+    let successes = lookups.iter().filter(|x| x.result().is_lookup()).count();
+    println!("Multi-Lookup successful results: {}/{}", successes, lookups.len());
+
+    let failures: Vec<_> = lookups.iter().filter(|x| !x.result().is_lookup()).collect();
+    println!("Multi-Lookup failed results: {:#?}", failures);
+}
