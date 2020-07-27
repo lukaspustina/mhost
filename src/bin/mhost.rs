b54a9c6dd8f3a6c83b4c7858986e6aa0d986017a
Lukas Pustina <lukas@pustina.de>
2020-07-27 21:25:38 +0200

    Add nameservers from file

--- src/bin/mhost.rs
+++ src/bin/mhost.rs
@@ -9,13 +9,13 @@ use futures::future::join_all;
 use log::{debug, LevelFilter};
 use nom::lib::std::collections::HashSet;
 
+use mhost::{IpNetwork, RecordType};
 use mhost::estimate::Estimate;
-use mhost::nameserver::{predefined, NameServerConfig, NameServerConfigGroup, Protocol};
-use mhost::output::summary::SummaryOptions;
+use mhost::nameserver::{NameServerConfig, NameServerConfigGroup, predefined, Protocol};
 use mhost::output::{Output, OutputConfig, OutputFormat};
+use mhost::output::summary::SummaryOptions;
 use mhost::resolver::{Lookups, MultiQuery, ResolverConfigGroup, ResolverGroup, ResolverGroupOpts, ResolverOpts};
 use mhost::statistics::Statistics;
-use mhost::{IpNetwork, RecordType};
 
 static SUPPORTED_RECORD_TYPES: &[&str] = &[
     "A", "AAAA", "ANAME", "CNAME", "MX", "NULL", "NS", "PTR", "SOA", "SRV", "TXT",
@@ -107,7 +107,7 @@ fn setup_clap() -> App<'static, 'static> {
             .use_delimiter(true)
             .require_delimiter(true)
             .value_delimiter(";")
-            .help("Adds nameserver for lookups; if non given, only system nameservers will be used")
+            .help("Adds nameserver for lookups")
         )
         .arg(Arg::with_name("predefined")
             .short("p")
@@ -129,6 +129,13 @@ fn setup_clap() -> App<'static, 'static> {
             .long("list-predefined")
             .conflicts_with("domain name")
             .help("Lists all predefined nameservers")
+        )
+        .arg(Arg::with_name("nameservers-from-file")
+            .short("f")
+            .long("nameservers-from-file")
+            .value_name("FILE")
+            .takes_value(true)
+            .help("Adds nameserver for lookups from file")
         )
         .arg(Arg::with_name("record types")
             .short("t")
@@ -262,7 +269,7 @@ fn ptr_query(ip_network: IpNetwork) -> Result<MultiQuery> {
     Ok(q)
 }
 
-fn record_types<'a, I: Iterator<Item = &'a str>>(record_types: I) -> Result<Vec<RecordType>> {
+fn record_types<'a, I: Iterator<Item=&'a str>>(record_types: I) -> Result<Vec<RecordType>> {
     let record_types: Vec<_> = record_types
         .map(str::to_uppercase)
         .map(|x| RecordType::from_str(&x))
@@ -291,10 +298,22 @@ async fn create_resolvers(
         resolver_opts.clone(),
         resolver_group_opts.clone(),
     )
-    .await
-    .context("Failed to create system resolvers")?;
+        .await
+        .context("Failed to create system resolvers")?;
     debug!("Created {} system resolvers.", system_resolvers.len());
 
+    let resolver_group: ResolverConfigGroup = load_nameservers(args, &mut system_resolvers).await?.into();
+    let resolvers = ResolverGroup::from_configs(resolver_group, resolver_opts, resolver_group_opts.clone())
+        .await
+        .context("Failed to load resolvers")?;
+    debug!("Created {} resolvers.", resolvers.len());
+
+    system_resolvers.merge(resolvers);
+
+    Ok(system_resolvers)
+}
+
+async fn load_nameservers(args: &ArgMatches<'_>, system_resolvers: &mut ResolverGroup) -> Result<NameServerConfigGroup> {
     let mut nameservers_group = NameServerConfigGroup::new(Vec::new());
     if let Some(configs) = args.values_of("nameservers") {
         let configs: Vec<_> = configs
@@ -321,16 +340,14 @@ async fn create_resolvers(
         debug!("Loaded {} nameservers.", nameservers.len());
         nameservers_group.merge(nameservers);
     }
+    if let Some(path) = args.value_of("nameservers-from-file") {
+        let nameservers = NameServerConfigGroup::from_file(&system_resolvers, path).await
+            .context("Failed to load nameservers from file")?;
+        debug!("Loaded {} nameservers from file.", nameservers.len());
+        nameservers_group.merge(nameservers);
+    }
 
-    let resolver_group: ResolverConfigGroup = nameservers_group.into();
-    let resolvers = ResolverGroup::from_configs(resolver_group, resolver_opts, resolver_group_opts.clone())
-        .await
-        .context("Failed to load resolvers")?;
-    debug!("Created {} resolvers.", resolvers.len());
-
-    system_resolvers.merge(resolvers);
-
-    Ok(system_resolvers)
+    Ok(nameservers_group)
 }
 
 fn load_resolver_group_opts(args: &ArgMatches) -> Result<ResolverGroupOpts> {
