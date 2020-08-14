ee178dc4a5aa4bda737034017415be493c04d1fe
Lukas Pustina <lukas@pustina.de>
2020-08-14 16:12:17 +0200

    Add IPv6 predefined name servers

--- src/nameserver/predefined.rs
+++ src/nameserver/predefined.rs
@@ -20,59 +20,170 @@ pub fn nameserver_configs() -> Vec<NameServerConfig> {
 }
 
 pub mod cloudflare {
-    use std::net::Ipv4Addr;
+    use std::net::{Ipv4Addr, Ipv6Addr};
 
     use crate::nameserver::NameServerConfig;
 
-    static IPV4: Ipv4Addr = Ipv4Addr::new(1, 1, 1, 1);
+    static IPV4_1: Ipv4Addr = Ipv4Addr::new(1, 1, 1, 1);
+    static IPV4_2: Ipv4Addr = Ipv4Addr::new(1, 0, 0, 1);
+    static IPV6_1: Ipv6Addr = Ipv6Addr::new(0x2606, 0x4700, 0x4700, 0x0, 0x0, 0x0, 0x0, 0x1111);
+    static IPV6_2: Ipv6Addr = Ipv6Addr::new(0x2606, 0x4700, 0x4700, 0x0, 0x0, 0x0, 0x0, 0x1001);
 
+    // IPv4 1
     pub fn udp() -> NameServerConfig {
-        NameServerConfig::udp_with_name((IPV4, 53), "Cloudflare".to_string())
+        NameServerConfig::udp_with_name((IPV4_1, 53), "Cloudflare 1".to_string())
     }
 
     pub fn tcp() -> NameServerConfig {
-        NameServerConfig::tcp_with_name((IPV4, 53), "Cloudflare".to_string())
+        NameServerConfig::tcp_with_name((IPV4_1, 53), "Cloudflare 1".to_string())
     }
 
     pub fn https() -> NameServerConfig {
-        NameServerConfig::https_with_name((IPV4, 443), "cloudflare-dns.com", "Cloudflare".to_string())
+        NameServerConfig::https_with_name((IPV4_1, 443), "cloudflare-dns.com", "Cloudflare 1".to_string())
     }
 
     pub fn tls() -> NameServerConfig {
-        NameServerConfig::tls_with_name((IPV4, 853), "cloudflare-dns.com", "Cloudflare".to_string())
+        NameServerConfig::tls_with_name((IPV4_1, 853), "cloudflare-dns.com", "Cloudflare 1".to_string())
+    }
+
+    // IPv4 2
+    pub fn udp_2() -> NameServerConfig {
+        NameServerConfig::udp_with_name((IPV4_2, 53), "Cloudflare 2".to_string())
+    }
+
+    pub fn tcp_2() -> NameServerConfig {
+        NameServerConfig::tcp_with_name((IPV4_2, 53), "Cloudflare 2".to_string())
+    }
+
+    pub fn https_2() -> NameServerConfig {
+        NameServerConfig::https_with_name((IPV4_2, 443), "cloudflare-dns.com", "Cloudflare 2".to_string())
+    }
+
+    pub fn tls_2() -> NameServerConfig {
+        NameServerConfig::tls_with_name((IPV4_2, 853), "cloudflare-dns.com", "Cloudflare 2".to_string())
+    }
+
+    // IPv6 1
+    pub fn udp6() -> NameServerConfig {
+        NameServerConfig::udp_with_name((IPV6_1, 53), "Cloudflare 1".to_string())
+    }
+
+    pub fn tcp6() -> NameServerConfig {
+        NameServerConfig::tcp_with_name((IPV6_1, 53), "Cloudflare 1".to_string())
+    }
+
+    pub fn https6() -> NameServerConfig {
+        NameServerConfig::https_with_name((IPV6_1, 663), "cloudflare-dns.com", "Cloudflare 1".to_string())
+    }
+
+    pub fn tls6_1() -> NameServerConfig {
+        NameServerConfig::tls_with_name((IPV6_1, 853), "cloudflare-dns.com", "Cloudflare 2".to_string())
+    }
+
+    // IPv6 2
+    pub fn udp6_2() -> NameServerConfig {
+        NameServerConfig::udp_with_name((IPV6_2, 53), "Cloudflare 2".to_string())
+    }
+
+    pub fn tcp6_2() -> NameServerConfig {
+        NameServerConfig::tcp_with_name((IPV6_2, 53), "Cloudflare 2".to_string())
+    }
+
+    pub fn https6_2() -> NameServerConfig {
+        NameServerConfig::https_with_name((IPV6_2, 663), "cloudflare-dns.com", "Cloudflare 2".to_string())
+    }
+
+    pub fn tls6_2() -> NameServerConfig {
+        NameServerConfig::tls_with_name((IPV6_2, 853), "cloudflare-dns.com", "Cloudflare 2".to_string())
     }
 }
 
 pub mod google {
-    use std::net::Ipv4Addr;
+    use std::net::{Ipv4Addr, Ipv6Addr};
 
     use crate::nameserver::NameServerConfig;
 
-    static IPV4: Ipv4Addr = Ipv4Addr::new(8, 8, 8, 8);
+    static IPV4_1: Ipv4Addr = Ipv4Addr::new(8, 8, 8, 8);
+    static IPV4_2: Ipv4Addr = Ipv4Addr::new(8, 8, 4, 4);
+    static IPV6_1: Ipv6Addr = Ipv6Addr::new(0x2001, 0x4860, 0x4860, 0x0, 0x0, 0x0, 0x0, 0x8888);
+    static IPV6_2: Ipv6Addr = Ipv6Addr::new(0x2001, 0x4860, 0x4860, 0x0, 0x0, 0x0, 0x0, 0x8844);
 
+    // IPv4 1
     pub fn udp() -> NameServerConfig {
-        NameServerConfig::udp_with_name((IPV4, 53), "Google".to_string())
+        NameServerConfig::udp_with_name((IPV4_1, 53), "Google 1".to_string())
     }
 
     pub fn tcp() -> NameServerConfig {
-        NameServerConfig::tcp_with_name((IPV4, 53), "Google".to_string())
+        NameServerConfig::tcp_with_name((IPV4_1, 53), "Google 1".to_string())
     }
 
     pub fn https() -> NameServerConfig {
-        NameServerConfig::https_with_name((IPV4, 443), "dns.google", "Google".to_string())
+        NameServerConfig::https_with_name((IPV4_1, 443), "dns.google", "Google 1".to_string())
     }
 
     pub fn tls() -> NameServerConfig {
-        NameServerConfig::tls_with_name((IPV4, 853), "dns.google", "Google".to_string())
+        NameServerConfig::tls_with_name((IPV4_1, 853), "dns.google", "Google 1".to_string())
+    }
+
+    // IPv4 2
+    pub fn udp_2() -> NameServerConfig {
+        NameServerConfig::udp_with_name((IPV4_2, 53), "Google 2".to_string())
+    }
+
+    pub fn tcp_2() -> NameServerConfig {
+        NameServerConfig::tcp_with_name((IPV4_2, 53), "Google 2".to_string())
+    }
+
+    pub fn https_2() -> NameServerConfig {
+        NameServerConfig::https_with_name((IPV4_2, 443), "dns.google", "Google 2".to_string())
+    }
+
+    pub fn tls_2() -> NameServerConfig {
+        NameServerConfig::tls_with_name((IPV4_2, 853), "dns.google", "Google 2".to_string())
+    }
+
+    // IPv6 1
+    pub fn udp6() -> NameServerConfig {
+        NameServerConfig::udp_with_name((IPV6_1, 53), "Google 1".to_string())
+    }
+
+    pub fn tcp6() -> NameServerConfig {
+        NameServerConfig::tcp_with_name((IPV6_1, 53), "Google 1".to_string())
+    }
+
+    pub fn https6() -> NameServerConfig {
+        NameServerConfig::https_with_name((IPV6_1, 443), "dns.google", "Google 1".to_string())
+    }
+
+    pub fn tls6() -> NameServerConfig {
+        NameServerConfig::tls_with_name((IPV6_1, 853), "dns.google", "Google 1".to_string())
+    }
+
+    // IPv6 2
+    pub fn udp6_2() -> NameServerConfig {
+        NameServerConfig::udp_with_name((IPV6_2, 53), "Google 2".to_string())
+    }
+
+    pub fn tcp6_2() -> NameServerConfig {
+        NameServerConfig::tcp_with_name((IPV6_2, 53), "Google 2".to_string())
+    }
+
+    pub fn https6_2() -> NameServerConfig {
+        NameServerConfig::https_with_name((IPV6_2, 443), "dns.google", "Google 2".to_string())
+    }
+
+    pub fn tls6_2() -> NameServerConfig {
+        NameServerConfig::tls_with_name((IPV6_2, 853), "dns.google", "Google 2".to_string())
     }
 }
 
 pub mod opennic {
-    use std::net::Ipv4Addr;
+    use std::net::{Ipv4Addr, Ipv6Addr};
 
     use crate::nameserver::NameServerConfig;
 
-    static IPV4: Ipv4Addr = Ipv4Addr::new(185, 121, 177, 177);
+    static IPV4: Ipv4Addr = Ipv4Addr::new(94, 103, 153, 176);
+    static IPV6: Ipv6Addr = Ipv6Addr::new(0x2a02,0x990,0x219,0x1,0xba,0x1337,0xcafe,0x3);
 
     pub fn udp() -> NameServerConfig {
         NameServerConfig::udp_with_name((IPV4, 53), "OpenNIC".to_string())
@@ -81,28 +192,91 @@ pub mod opennic {
     pub fn tcp() -> NameServerConfig {
         NameServerConfig::tcp_with_name((IPV4, 53), "OpenNIC".to_string())
     }
+
+    pub fn udp6() -> NameServerConfig {
+        NameServerConfig::udp_with_name((IPV6, 53), "OpenNIC".to_string())
+    }
+
+    pub fn tcp6() -> NameServerConfig {
+        NameServerConfig::tcp_with_name((IPV6, 53), "OpenNIC".to_string())
+    }
 }
 
 pub mod quad9 {
-    use std::net::Ipv4Addr;
+    use std::net::{Ipv4Addr, Ipv6Addr};
 
     use crate::nameserver::NameServerConfig;
 
-    static IPV4: Ipv4Addr = Ipv4Addr::new(9, 9, 9, 9);
+    static IPV4_1: Ipv4Addr = Ipv4Addr::new(9, 9, 9, 9);
+    static IPV4_2: Ipv4Addr = Ipv4Addr::new(149, 112, 112, 112);
+    static IPV6_1: Ipv6Addr = Ipv6Addr::new(0x2620,0xfe, 0x0, 0x0, 0x0, 0x0, 0x0,0xfe);
+    static IPV6_2: Ipv6Addr = Ipv6Addr::new(0x2620,0xfe, 0x0, 0x0, 0x0, 0x0, 0x0,0x9);
 
+    // IPv4 1
     pub fn udp() -> NameServerConfig {
-        NameServerConfig::udp_with_name((IPV4, 53), "Quad9".to_string())
+        NameServerConfig::udp_with_name((IPV4_1, 53), "Quad9 1".to_string())
     }
 
     pub fn tcp() -> NameServerConfig {
-        NameServerConfig::tcp_with_name((IPV4, 53), "Quad9".to_string())
+        NameServerConfig::tcp_with_name((IPV4_1, 53), "Quad9 1".to_string())
     }
 
     pub fn https() -> NameServerConfig {
-        NameServerConfig::https_with_name((IPV4, 443), "dns.quad9.net", "Quad9".to_string())
+        NameServerConfig::https_with_name((IPV4_1, 443), "dns.quad9.net", "Quad9 1".to_string())
     }
 
     pub fn tls() -> NameServerConfig {
-        NameServerConfig::tls_with_name((IPV4, 853), "dns.quad9.net", "Quad9".to_string())
+        NameServerConfig::tls_with_name((IPV4_1, 853), "dns.quad9.net", "Quad9 1".to_string())
+    }
+
+    // IPv4 2
+    pub fn udp_2() -> NameServerConfig {
+        NameServerConfig::udp_with_name((IPV4_2, 53), "Quad9 2".to_string())
+    }
+
+    pub fn tcp_2() -> NameServerConfig {
+        NameServerConfig::tcp_with_name((IPV4_2, 53), "Quad9 2".to_string())
+    }
+
+    pub fn https_2() -> NameServerConfig {
+        NameServerConfig::https_with_name((IPV4_2, 443), "dns.quad9.net", "Quad9 2".to_string())
+    }
+
+    pub fn tls_2() -> NameServerConfig {
+        NameServerConfig::tls_with_name((IPV4_2, 853), "dns.quad9.net", "Quad9 2".to_string())
+    }
+
+    // IPv6 1
+    pub fn udp6() -> NameServerConfig {
+        NameServerConfig::udp_with_name((IPV6_1, 53), "Quad9 1".to_string())
+    }
+
+    pub fn tcp6() -> NameServerConfig {
+        NameServerConfig::tcp_with_name((IPV6_1, 53), "Quad9 1".to_string())
+    }
+
+    pub fn https6() -> NameServerConfig {
+        NameServerConfig::https_with_name((IPV6_1, 663), "dns.quad9.net", "Quad9 1".to_string())
+    }
+
+    pub fn tls6() -> NameServerConfig {
+        NameServerConfig::tls_with_name((IPV6_1, 853), "dns.quad9.net", "Quad9 1".to_string())
+    }
+
+    // IPv6 2
+    pub fn udp6_2() -> NameServerConfig {
+        NameServerConfig::udp_with_name((IPV6_2, 53), "Quad9 2".to_string())
+    }
+
+    pub fn tcp6_2() -> NameServerConfig {
+        NameServerConfig::tcp_with_name((IPV6_2, 53), "Quad9 2".to_string())
+    }
+
+    pub fn https6_2() -> NameServerConfig {
+        NameServerConfig::https_with_name((IPV6_2, 663), "dns.quad9.net", "Quad9 2".to_string())
+    }
+
+    pub fn tls6_2() -> NameServerConfig {
+        NameServerConfig::tls_with_name((IPV6_2, 853), "dns.quad9.net", "Quad9 2".to_string())
     }
 }
