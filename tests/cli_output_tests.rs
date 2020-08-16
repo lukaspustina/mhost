31dd2b7f92960a8adf17ac4fe315adb5b4cf7681
Lukas Pustina <lukas@pustina.de>
2020-08-16 21:26:43 +0200

    Add lit based CLI output tests

---
+++ tests/cli_output_tests.rs
@@ -0,0 +1,20 @@
+use lit;
+use std::env;
+
+const CRATE_PATH: &'static str = env!("CARGO_MANIFEST_DIR");
+const DEBUG_TARGET_PATH: &'static str = "target/debug";
+
+fn mhost_bin() -> String {
+    format!("{}/{}/mhost{}", CRATE_PATH, DEBUG_TARGET_PATH, env::consts::EXE_SUFFIX)
+}
+
+#[test]
+fn cli_output_tests() {
+    lit::run::tests(lit::event_handler::Default::default(), |config| {
+        config.add_search_path("tests/lit");
+        config.add_extension("output");
+        config.constants.insert("mhost_bin".to_owned(), mhost_bin());
+        config.constants.insert("mhost_version".to_owned(), env!("CARGO_PKG_VERSION").to_owned());
+    })
+    .expect("cli output tests failed");
+}
