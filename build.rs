1a72b78d05879844b95c6541587df8c90ea2e553
Lukas Pustina <lukas@pustina.de>
2021-02-27 14:56:17 +0100

    Fix build.rs for cargo publish and docs.rs

--- build.rs
+++ build.rs
@@ -8,15 +8,21 @@
 use clap::Shell;
 use std::env;
 use std::fs;
-use std::path::Path;
+use std::process;
 
 #[allow(dead_code)]
 #[path = "src/app/cli_parser.rs"]
 mod cli_parser;
 
 fn main() {
-    let root_dir = env::var_os("CARGO_MANIFEST_DIR").expect("Cargo output directory environment variable is not set.");
-    let output_dir = Path::new(&root_dir).join("contrib").join("shell-completions");
+    // OUT_DIR is set by Cargo and is the only place which is allowed to be written to during compilation for 'cargo publihs' and docs.rs.
+    let output_dir = match env::var_os("OUT_DIR") {
+        Some(outdir) => outdir,
+        None => {
+            eprintln!("Cargo output directory environment variable is not set: Cannot continue. Aborting");
+            process::exit(1);
+        }
+    };
     fs::create_dir_all(&output_dir).expect("failed to create output directory");
 
     // Create Shell completions
