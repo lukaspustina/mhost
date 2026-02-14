f54a9176e518ccc42f7aa8fef8195783b0ae3856
Lukas Pustina <lukas@pustina.de>
2026-02-14 10:31:14 +0100

    Add Homebrew tap auto-update and improve formula
    Add repository dispatch step to release workflow to automatically
    update the lukaspustina/homebrew-mhost tap on new releases. Fix
    formula: use explicit refs/tags/ URL, specify head branch, replace
    network-dependent test with offline version check.

--- distribution/homebrew/mhost.rb
+++ distribution/homebrew/mhost.rb
@@ -1,10 +1,10 @@
 class Mhost < Formula
   desc "More than host - A modern take on the classic host DNS lookup utility"
   homepage "https://mhost.pustina.de"
-  url "https://github.com/lukaspustina/mhost/archive/v0.3.2.tar.gz"
+  url "https://github.com/lukaspustina/mhost/archive/refs/tags/v0.3.2.tar.gz"
   sha256 "3b4b2e292c6cef00cb8b71e1b2adf01c0d4a0e02daa671fcc72522413b336047"
   license any_of: ["MIT", "Apache-2.0"]
-  head "https://github.com/lukaspustina/mhost.git"
+  head "https://github.com/lukaspustina/mhost.git", branch: "master"
 
   depends_on "rust" => :build
 
@@ -12,13 +12,12 @@ class Mhost < Formula
     system "cargo", "install", "--features", "app", *std_cargo_args
 
     out_dir = Dir["target/release/build/mhost-*/out"].first
-    # man1.install "#{out_dir}/man.1"
     bash_completion.install "#{out_dir}/mhost.bash"
     fish_completion.install "#{out_dir}/mhost.fish"
     zsh_completion.install "#{out_dir}/_mhost"
   end
 
   test do
-    system "#{bin}/mhost", "lookup", "mhost.pustina.de"
+    assert_match "mhost", shell_output("#{bin}/mhost --version")
   end
 end
